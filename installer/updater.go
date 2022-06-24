283fa4854b23faea1ed1c30252be80d21f276ac0
Martin Starman <martin.starman@kit.edu>
2022-06-24 14:01:05 +0200

    fix v1.0

--- installer/updater.go
+++ installer/updater.go
@@ -13,8 +13,9 @@ func run_update() {
 	file_text := strings.Split(string(project_list), "\n")
 	for _, user := range file_text {
 		if user != "" {
+			cmd(fmt.Sprintf("systemctl stop eln_instance_%s.service", user))
 			copy_to_user(user)
-			cmd(fmt.Sprintf("systemctl restart eln_instance_%s.service", user))
+			cmd(fmt.Sprintf("systemctl start eln_instance_%s.service", user))
 		}
 	}
 }
